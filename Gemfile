208b8601ab8e4716b85840e156dcd8b358c86f27
ningluo <nluo@bidz.com>
2013-01-21 13:51:36 -0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.11'
+
+gem 'mysql2'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '98fe7b78be9e97e899d44271328542acddb732e7c9e18d05bb5c22540141b9af95f3f555550908a5405c1e4d362d6ee51fa165027647c460be9b72fc46e365cc'
